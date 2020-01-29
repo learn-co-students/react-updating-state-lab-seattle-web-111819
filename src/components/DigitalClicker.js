a41a49bfed9997831d8b1d7c0b1b2dfc91485c70
4myc <missch3n@gmail.com>
2020-01-29 01:57:47 -0800

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,30 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react'
+
+class DigitalClicker extends Component {
+
+    // Initial state property called timesClicked, which is initially defined as 0.
+    constructor() {
+        super()
+        this.state = {
+            timesClicked: 0
+        }
+    }
+
+    // Whenever the button is clicked, update the state by incrementing the timesClicked by 1.
+    handleClick = () => {
+        this.setState(prevState => ({
+            timesClicked: prevState.timesClicked +1
+        }))
+    }
+
+    render() {
+        return (
+            // Renders out a button with a label that shows the timesClicked value. 
+            // This means that, at the start, your button should just say 0.
+            <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+        )
+    }
+}
+
+export default DigitalClicker
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,58 @@
 // Code YouTubeDebugger Component Here
+import React, { Component } from 'react'
+
+class YouTubeDebugger extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+                bitrate: 8,
+                video: {
+                    resolution: '1080p'
+                    }
+            }
+        }
+    }
+
+    // Use ... (spread operator) to create a deep merge --> a merge will happen recursively, leaving any unchanged properties intact. 
+    handleBitrate = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                bitrate: 12
+            }
+        })
+    }
+
+    handleResolution = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                video: {
+                    ...this.state.settings.video,
+                    resolution: '720p'
+                }
+            }
+
+        })
+    }
+
+    render() {
+        return (
+            <div>
+            {/* button with the class 'bitrate'. */}
+            {/* Clicking this button changes the settings.bitrate state property to 12. */}
+            <button className="bitrate" onClick={this.handleBitrate}>Change Bitrate</button>
+
+            {/* button with the class 'resolution'.  */}
+            {/* Clicking this button changes the settings.video.resolution state property to '720p'. */}
+            <button className="resolution" onClick={this.handleResolution}>Change Resolution</button>
+            </div>
+        )
+    }
+}
+
+export default YouTubeDebugger
